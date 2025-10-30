e67d7a25a20d5668a09d111bc447243cbf5e13d5
adolf-codler <pramanrohilla7472@gmail.com>
2025-10-31 02:14:43 +0530

    test

---
+++ Knob/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Gay  = () => {
+  return (
+    <View>
+      <Text>Gay </Text>
+    </View>
+  )
+}
+
+export default Gay 
